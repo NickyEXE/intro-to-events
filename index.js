5cad73dbb1780f90a1dd9425448007694dcfe9d3
NickyEXE <dovern42@gmail.com>
2020-09-03 19:02:06 -0400

    event delegation

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const emojiForm = document.getElementById("emoji-form")
 const initialEmojis = ["🍓", "🍕", "🥔", "😋", "🚽", "💪"]
 initialEmojis.forEach(addEmoji)
 
+
+
 // document.addEventListener("click", (event) => {
 //     event.target.innerText = "🌮"
 // })
@@ -13,15 +15,27 @@ tacoButton.addEventListener("click", () => {
   addEmoji("🌮")
 })
 
+// Closures
+// function addEmoji(emoji){
+//   const li = document.createElement("li")
+//   li.innerText = emoji
+//   ul.appendChild(li)
+//   li.addEventListener("dblclick", () => li.remove())
+// }
+
 function addEmoji(emoji){
-  const li = document.createElement("li")
-  li.innerText = emoji
-  ul.appendChild(li)
+  ul.innerHTML += `<li>${emoji}</li>`
 }
 
-document.querySelector("li").addEventListener("click", (e) => {
-  e.target.style.backgroundColor = "yellow"
+ul.addEventListener("dblclick", (e) => {
+  if (e.target.tagName === "LI"){
+    e.target.remove()
+  }
 })
+
+// document.querySelector("li").addEventListener("click", (e) => {
+//   e.target.style.backgroundColor = "yellow"
+// })
 
 emojiForm.addEventListener("submit", (e) => {
   e.preventDefault()
