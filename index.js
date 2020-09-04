bf1968da4a3eede5f50bd0ab7a1449d8ae4eaae1
NickyEXE <dovern42@gmail.com>
2020-09-04 16:52:00 -0400

    end of lecture

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const tacoButton = document.querySelector("#taco")
 const ul = document.getElementById("blobs")
 const emojiForm = document.getElementById("emoji-form")
+const commentForm = document.getElementById("comment-form")
+const comments = document.getElementById("comments")
 
 const initialEmojis = ["🍓", "🍕", "🥔", "😋", "🚽", "💪"]
 initialEmojis.forEach(addEmoji)
@@ -19,13 +21,23 @@ tacoButton.addEventListener("click", () => {
 // function addEmoji(emoji){
 //   const li = document.createElement("li")
 //   li.innerText = emoji
+//   let counter = 0
 //   ul.appendChild(li)
-//   li.addEventListener("dblclick", () => li.remove())
+//   li.addEventListener("click", () => {
+//     counter ++
+//     console.log(`${emoji} says ${counter}`)
+//   })
 // }
 
 function addEmoji(emoji){
-  ul.innerHTML += `<li>${emoji}</li>`
+  ul.innerHTML += `<li data-emoji=${emoji}>${emoji}</li>`
 }
+
+ul.addEventListener("click", (e)=> {
+  if (e.target.dataset.emoji === "🌮"){
+    alert("YUMMMMM")
+  }
+})
 
 ul.addEventListener("dblclick", (e) => {
   if (e.target.tagName === "LI"){
@@ -42,3 +54,22 @@ emojiForm.addEventListener("submit", (e) => {
   addEmoji(e.target.emoji.value)
   emojiForm.reset()
 })
+
+commentForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  addComment(commentForm.comment.value, commentForm.initialClicks.value)
+  commentForm.reset()
+})
+
+function addComment(comment, numClicks){
+  comments.innerHTML += `<li data-comment=${comment} data-num-clicks=${numClicks}>${numClicks}: ${comment}</li>`
+}
+
+comments.addEventListener("click", (e) => {
+  if (e.target.tagName == "LI"){
+    e.target.dataset.numClicks++
+    e.target.innerText = `${e.target.dataset.numClicks}: ${e.target.dataset.comment}`
+  }
+})
+
+{/* <li>47: This is how many times I've been clicked</li> */}
